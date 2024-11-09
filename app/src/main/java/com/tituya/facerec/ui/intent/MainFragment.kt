aa32b9d46e506fddb0493d01888b7037674799e0
Benjamin <bgiraud@protonmail.com>
2024-11-09 01:24:04 +0100

    add material theme

--- app/src/main/java/com/tituya/facerec/ui/intent/MainFragment.kt
+++ app/src/main/java/com/tituya/facerec/ui/intent/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.tituya.facerec.R
 import com.tituya.facerec.databinding.FragmentMainBinding
 
@@ -17,6 +19,14 @@ class MainFragment : Fragment() {
     ): View? {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        val picker =
+            MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(10)
+                .setTitleText("Select Appointment time")
+                .build()
 
         return view
     }
